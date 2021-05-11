268a6358863995d7a6dc639a5c25160f838c27f6
unknown <amirthayazhini.m@gmail.com>
2021-05-11 09:48:38 +0530

    data changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=' + latitude + ',' + longitude + '&units=f'
 
@@ -10,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location' , undefined)
             
         } else{
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " +body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0].summary + ". It is currently " +body.current.temperature + " degress out." + body.current.weather_descriptions.humidity) 
         }
     })
 }
